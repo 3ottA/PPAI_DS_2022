5503a18979ab37a327484b77e9d42250dd2bd67f
3ottA <bottafrancisco@hotmail.com>
2022-07-04 15:17:47 -0300

    FinCU

--- PantallaIngresoRT.py
+++ PantallaIngresoRT.py
@@ -23,52 +23,52 @@ class PantallaIngresoRT:
             "Tipo de Recurso Desc"))
 
         while True:
-            seleccion = input("Seleccione el RT ingresando su NumeroRT : ")
+            seleccion = input("\nSeleccione el RT ingresando su NumeroRT : ")
             if seleccion.isnumeric():
                 rtSeleccionado = tabla.loc[tabla['NumeroRT'] == int(seleccion)]
                 if len(rtSeleccionado) != 0:
-                    print("Recurso seleccionado:\n",
+                    print("\nRecurso seleccionado:\n",
                           rtSeleccionado)
-                    fechaFin = input("Definir una FechaFin : ")
+                    fechaFin = input("\nDefinir una FechaFin : ")
                     if fechaFin.isnumeric():
                         ingresarRazon = input(
-                            "Ingresar razon de mantenimiento: ")
+                            "\nIngresar razon de mantenimiento: ")
                         break
                     else:
-                        print("por favor ingresar fecha correcta")
+                        print("\npor favor ingresar fecha correcta")
                 else:
-                    print("No hay RT disponible con el Numero : ", seleccion)
+                    print("\nNo hay RT disponible con el Numero : ", seleccion)
             else:
-                print("por favor ingresar Numero correcto")
+                print("\npor favor ingresar Numero correcto")
+
         gestor.tomarSeleccionFechaFin(int(fechaFin))
         gestor.tomarRazon(ingresarRazon)
         seleccionado = rtSeleccionado.iloc[0]["objeto"]
         gestor.tomarSeleccionRT(seleccionado)
         #print("Seleccionado", seleccionado)
+
         turnosConfirmadoPendiente = gestor.buscarTurnosConfirmadoPendiente(
             seleccionado)
         listaTurnosCientificoCancelado = turnosConfirmadoPendiente.copy()
         #print( listaTurnosCientificoCancelado)
+
         datosPandas = copy.deepcopy(turnosConfirmadoPendiente)
         for i in range(len(datosPandas)):
             datosPandas[i][2] = datosPandas[i][2].getApellidoNombre()
         # print(datosPandas)
+
         tablaTurno = pd.DataFrame(datosPandas, columns=[
             "DiaInicio", "DiaFin", "Cientifico", "objetoTurno"],)
         print(tablaTurno.loc[:, tablaTurno.columns != "objetoTurno"].sort_values(
             "Cientifico"))
-        inp = eval(
-            input("Desea Confirmar el ingreso a Mantenimiento?\n ingresar True o False: "))
+
         # gestor.tomarConfirmacionIngreso(inp,datosPandas)
         self.solicitarOpcionDeNotificacion(gestor)
-        #print("\nESTA es?", listaTurnosCientificoCancelado, "\n\n")
+        inp = eval(
+            input("Desea Confirmar el ingreso a Mantenimiento?\n ingresar True o False: "))
+
         gestor.tomarConfirmacionDeIngreso(
             inp, listaTurnosCientificoCancelado, seleccionado)
-        # if gestor.confirmacionIngreso == True:
-        #     print("A")
-        #     opNot =self.solicitarOpcionDeNotificacion(gestor)
-        # else:
-        #     print("No aceptado")
 
     def habilitarVentana():
         pass
